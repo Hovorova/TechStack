0e000da4d89d6ba83c59403f5629f56e2cfc85af
Hovorova <anya_gov_09@ukr.net>
2022-01-17 16:42:24 +0200

    Updated

--- TechStack_Task1/src/test/java/io/techstack/pages/MainPage.java
+++ TechStack_Task1/src/test/java/io/techstack/pages/MainPage.java
@@ -1,11 +1,12 @@
 package io.techstack.pages;
 
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+@Getter
 public class MainPage {
 
     @FindBy(xpath = "//*[@id=\"profile-menu-trigger--title\"]")
