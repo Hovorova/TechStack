aac6e15f07d7f1f968f0eb6ff6774263abfd5ecb
Hovorova <anya_gov_09@ukr.net>
2022-01-18 16:30:59 +0200

    using all functions and operations from presentation

---
+++ TechStack_Task1/src/test/java/io/techstack/XPaths/XPaths.java
@@ -0,0 +1,58 @@
+package io.techstack.XPaths;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XPaths {
+    @FindBy(xpath = ".//div[starts-with(text(), 'Very good')]")
+    private List<WebElement> hotelsWithVeryGoodRate;
+
+    @FindBy(xpath = ".//div[@class='bui-group__item']//parent::*")
+    private WebElement mainMenu;
+
+    @FindBy(xpath = ".//div[@data-testid='review-score']//div[@aria-label and text()>='9']")
+    private List<WebElement> hotelsWithRateOver9;
+
+    @FindBy(xpath = ".//div[@data-testid='property-card'] //*[text()='FREE cancellation' or text()='No prepayment needed']")
+    private List<WebElement> hotelsWithFreeCancellationOrNoPrepayment;
+
+    @FindBy(xpath = ".//div[contains(text(),'Almost done!')]")
+    private WebElement successfulBanner;
+
+    @FindBy(xpath = ".//div[contains(@class,'bui-group bui')]//preceding-sibling::a[contains(@class,'bui-button bui-button--sec')]")
+    private WebElement registerButton;
+
+    @FindBy(xpath = ".//div[@class='bui-group__item'][last()]")
+    private WebElement signInButton;
+
+    @FindBy(xpath = ".//div[contains(@class,'bui-group bui-button')]//descendant::button")
+    private WebElement languageButton;
+
+    @FindBy(xpath = ".//div[@class='bui-header__main']/following-sibling::*")
+    private WebElement mainMenuWithoutLogo;
+
+    @FindBy(xpath = ".//li[@class='bui-tab__item']//ancestor::nav[contains(@class,'bui-tab bui-head')]")
+    private WebElement underMenuPartnersLinks;
+
+    @FindBy(xpath = "//a[@role='button']/preceding::div[@data-testid='property-card']")
+    private WebElement hotelsInKharkiv;
+
+    @FindBy(xpath = "//div[count(input)] ")
+    private WebElement inputsOnPage;
+
+    @FindBy(xpath = ".//input[not(@type=\"password\"=false())]")
+    private WebElement enterPassword;
+
+    @FindBy(xpath = "//input | //button")
+    private WebElement findsAllInputsOrButtonsons;
+
+    @FindBy(xpath = "//div[@aria-label and text() mod 3]")
+    private WebElement usingMod;
+}
